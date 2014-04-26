ab03850a5c1ec14af7f3c5d87ca372d5a34f7c22
sean-broomfield <seanbroomfield@Seans-MacBook-Pro.local>
2014-04-26 16:09:39 -0400

    Merge branch 'master' of https://github.com/sean-broomfield/SOS
    Conflicts:
    Job.cpp
    Job.h
    os.cpp

---
+++ DrumManager.h
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+#include <list>
+#include <cstdlib>
+#include "Job.h"
+
+using namespace std;
+
+class DrumManager {
+public:
+    DrumManager();
+    
+    DrumManager(list<Job> jobs) {
+        this->jobs = jobs;
+    }
+    
+    /*Queues Jobs to be processed. */
+    Job queueJob(Job job, int direction) {
+        job.setDirection(direction);
+        drumQueue.push(job);
+        return job;
+    }
+    
+    /*Checks to see if jobs are in memory.
+     If job is in memory it sets "setInMemory"
+     variable to true. If it is not, it sets it
+     to false and removes the job from the drum
+     and sets "setInMemory" to false.*/
+    Job swap(list<Job> jobs) {
+        Job job = drumQueue.front();
+        if(job.getDirection() == 1) {
+            job.setIsInMemory(true);
+        } else {
+            //remove Job from memory
+            job.setLocation(-1);
+            job.setIsInMemory(false);
+        }
+        drumQueue.pop();
+        return job;
+    }
+    
+    /*Completes the drmint for each job if they are in memory
+    returns a job to os.cpp. This job that is returned is the new drumJob in
+    os.cpp.*/
+    Job manageDrum(Job drumJob) {
+        //Must add "for all jobs in list jobs" clause
+        Job j;
+        if(j.getLocation() == -1) {
+            if(j.findMemory()) {
+                queueJob(j, 1);
+            }
+        }
+        if (drumQueue.size() > 0 && drumQueue.front().getNumber() != drumJob.getNumber()) {
+            Job newJob = drumQueue.front();
+            if(newJob.getDirection() == 1) {
+                //sos->siodrum(newJob.getNumber(), newJob.getSize(), newJob.getLocation(), 0);
+            } else {
+                //sos->siodrum(newJob.getNumber(), newJob.getSize(),    newJob.getLocation(), 1);
+            }
+            return newJob;
+        }
+        return drumJob;
+    }
+    
+    
+    
+private:
+    //Queues jobs that have initiated a drmint
+    queue<Job> drumQueue;
+    
+    //List of all jobs that have entered the system
+    list<Job> jobs;
+};
---
+++ ioManager.h
@@ -0,0 +1,76 @@
+#ifndef IOMANAGER_H
+#define IOMANAGER_H
+
+#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include "Job.h"
+
+using namespace std;
+
+class ioManager {
+public:
+    ioManager() {}
+    
+    Job requestIO(Job job){
+        ioQueue.push_back(job);
+        
+        if(ioQueue.size() == 1) {
+            //CALL SOS SIODISK
+            //siodisk(job.getNumber());
+            return job;
+        } else {
+            return ioQueue.front();
+        }
+    }
+   
+    
+    Job IOmanage(Job job){
+        Job job1;
+        if (ioQueue.size() > 0) {
+            Job newJob = ioQueue.front();
+            //CALL SOS SIODISK
+            //sos.siodisk(newJob.getNumber());
+            newJob.setLatched(true);
+            return newJob;
+        }
+        return job1;
+    }
+    
+    Job finishIO(Job job) {
+        job.setLatched(false);
+        //REMOVE FIRST OCCURENCE OF THIS JOB
+        ioQueue.pop_front();
+        
+        if(job.isBlocked() && (this->contains(job))){
+            job.setBlocked(false);
+        }
+        job = IOmanage(job);
+        return job;
+    }
+    
+    
+    
+    bool contains(Job job) {
+        for(deque<Job>::iterator iter = ioQueue.begin(); iter != ioQueue.end(); iter++) {
+            if (iter->getNumber() == job.getNumber())
+                return true;
+        }
+        return false;
+    }
+    
+    /*
+    void PrintIoTable() {
+        cout << "-------------------------------------------------" << endl;
+        cout << "| number \t| location \t| size \t| in memory? \t|" << endl;
+        for(deque<Job>::iterator iter = ioQueue.begin(); iter != ioQueue.end(); iter++) {
+            cout << "| " << *iter->getNumber() << "\t\t|" << *iter->getLocation() << "\t\t|" << *iter->getSize() << "\t|" << *iter->isInMemory() << "\t\t|" << endl;
+        }
+        cout << "-------------------------------------------------" << endl;
+    }
+     */
+private:
+    deque<Job> ioQueue;
+};
+
+#endif
